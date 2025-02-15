9680688c9b424c74ada42e6fd1357f7d1d01e4ea
Aaron Wright <aarnwri@gmail.com>
2025-02-14 23:24:44 -0800

    Add TicTacToe::board

---
+++ lib/tic_tac_toe/board.rb
@@ -0,0 +1,7 @@
+module TicTacToe
+class Board
+  def initialize
+    @tiles = Array.new(3) { Array.new(3) { nil } }
+  end
+end # class Board
+end # module TicTacToe
--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -4,13 +4,16 @@ class Game
     @player_1 = player_1
     @player_2 = player_2
 
+    @board    = Board.new
     @started  = false
     @finished = false
   end
 
   def play
+    # NOTE: This is just to debug...
     puts "now playing the game: #{@player_1.inspect}"
     puts "also: #{@player_2.inspect}"
+    puts "using board: #{@board.inspect}"
   end
 end # class Game
 end # module TicTacToe
